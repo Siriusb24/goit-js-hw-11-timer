4bb71f785783d098f02f80c66fca5b82a9d870dd
Siriusb24 <Siriusb2324@gmail.com>
2021-09-16 00:02:53 +0300

    update-index.js

---
+++ index.js
@@ -0,0 +1,68 @@
+const daysToFinish = document.querySelector('[data-value="days"]')
+const hoursToFinish = document.querySelector('[data-value="hours"]')
+const minsToFinish = document.querySelector('[data-value="mins"]')
+const secToFinish = document.querySelector('[data-value="secs"]')
+
+class CountdownTimer{
+    constructor(coronaFinish){
+      this.coronaFinish = coronaFinish.targetDate.getTime()
+      this.intervalID = null
+      this.finishTime = 0
+    }
+    ohNo() {
+          this.intervalID = setInterval(() => {
+          let currentDate = Date.now()
+          this.finishTime = this.coronaFinish - currentDate
+    
+          this.insertData(daysToFinish, this.getDaysCount(this.finishTime))
+          this.insertData(hoursToFinish, this.getHoursCount(this.finishTime))
+          this.insertData(minsToFinish, this.getMinsCount(this.finishTime))
+          this.insertData(secToFinish, this.getSecondsCount(this.finishTime))
+        }, 1000)
+      }
+    weDidIt() {
+        clearInterval(this.intervalID)
+        this.clearClockFace()
+      }
+      clearClockFace() {
+        daysToFinish.textContent = '00'
+        hoursToFinish.textContent = '00'
+        minsToFinish.textContent = '00'
+        secToFinish.textContent = '00'
+      }
+
+      toString(value, num, symbol) {
+        return String(value).padStart(num, symbol)
+      }
+      getDaysCount(time) {
+        return this.toString(Math.floor(time / (1000 * 60 * 60 * 24)), 3, ' ')
+      }
+      getHoursCount(time) {
+        return this.toString(
+          Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+          2,
+          '0',
+        )
+      }
+      getMinsCount(time) {
+        return this.toString(
+          Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
+          2,
+          '0',
+        )
+      }
+      getSecondsCount(time) {
+        return this.toString(Math.floor((time % (1000 * 60)) / 1000), 2, '0')
+      }
+      insertData(place, value) {
+        place.textContent = value
+      }
+
+}
+
+  const myTimer = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Nov 31, 2021'),
+  });
+  myTimer.ohNo(myTimer.targetDate);
+  console.log(myTimer)
